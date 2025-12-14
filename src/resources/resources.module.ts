73cef084cbd2c008e7d61054289e6ec7ba622b1f
Tez-Tez-Tez <tezclaudia4@gmail.com>
2025-12-13 20:46:06 -0500

    fix: correct import path for BookingRulesModule in resources.module.ts

--- src/resources/resources.module.ts
+++ src/resources/resources.module.ts
@@ -4,7 +4,7 @@ import { ResourcesService } from './resources.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ResourcesEntity } from './entity/resources.entity';
 import { BookingsEntity } from 'src/bookings/entity/bookings.entity';
-import { BookingRulesModule } from './booking-rules/booking-rules.module';
+import { BookingRulesModule } from 'src/booking-rules/booking-rules.module';
 import { RoomsEntity } from 'src/rooms/entity/rooms.entity';
 import { VehiclesEntity } from 'src/vehicles/entity/vehicles.entity';
 import { EquipmentEntity } from 'src/equipment/entity/equipment.entity';
